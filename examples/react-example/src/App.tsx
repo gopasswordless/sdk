833c209b47403dc9e83eb021d9bcac86c73c12e0
Joe Bloxsome <joe@bloxso.me>
2024-02-24 22:17:48 +0000

    add logo presets

--- examples/react-example/src/App.tsx
+++ examples/react-example/src/App.tsx
@@ -6,10 +6,60 @@ import {
   GoPasswordlessDrawerComponent,
 } from "@gopasswordless/sdk";
 
+const presets = [
+  {
+    name: "GoPasswordless Light",
+    theme: "light",
+    logo: "https://gopasswordless.s3.eu-west-2.amazonaws.com/images/logo.png",
+    colour: "#027bff",
+  },
+  {
+    name: "GoPasswordless Dark",
+    theme: "dark",
+    logo: "https://gopasswordless.s3.eu-west-2.amazonaws.com/images/logo-light.png",
+    colour: "#027bff",
+  },
+  {
+    name: "SpaceX",
+    theme: "light",
+    logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6XOfRqvwz3RsgIsGley3e1a45vxFMOjGWZ51yyGZM&s",
+    colour: "#005288",
+  },
+  {
+    name: "SpaceX Dark",
+    theme: "dark",
+    logo: "https://spacex-microsite.netlify.app/images/spacex-logo-big.png",
+    colour: "#10578c",
+  },
+  {
+    name: "Tesla",
+    theme: "light",
+    logo: "https://1000logos.net/wp-content/uploads/2018/02/Font-Tesla-logo.jpg",
+    colour: "#000000",
+  },
+];
+
 export const App = (): JSX.Element => {
   const [mode, setMode] = useState<"light" | "dark" | "glass">("light");
+  const [logoUrl, setLogoUrl] = useState<string>(
+    "https://gopasswordless.s3.eu-west-2.amazonaws.com/images/logo.png"
+  );
+  const [primaryColour, setPrimaryColour] = useState<string>("#027bff");
   const [showContainer, setShowContainer] = useState(true);
   const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+  const [selectedPreset, setSelectedPreset] = useState<string>(
+    "GoPasswordless Light"
+  );
+
+  const selectPreset = (preset: string): void => {
+    const selectedPreset = presets.find((p) => p.name === preset);
+    if (selectedPreset) {
+      setMode(selectedPreset.theme as "light" | "dark" | "glass");
+      setLogoUrl(selectedPreset.logo);
+      setPrimaryColour(selectedPreset.colour);
+      setSelectedPreset(preset);
+    }
+  };
 
   useEffect(() => {
     const handleResize = () => {
@@ -31,11 +81,8 @@ export const App = (): JSX.Element => {
       settings={{
         appId: process.env.REACT_APP_GOPASSWORDLESS_APP_ID || "",
         appName: "GoPasswordless Demo",
-        appLogo:
-          mode === "dark" || mode === "glass"
-            ? "https://gopasswordless.s3.eu-west-2.amazonaws.com/images/logo-light.png"
-            : "https://gopasswordless.s3.eu-west-2.amazonaws.com/images/logo.png",
-        primaryColour: "#027bff",
+        appLogo: logoUrl,
+        primaryColour,
         theme: mode,
         privacyUrl:
           "https://gopasswordless.s3.eu-west-2.amazonaws.com/privacy.html",
@@ -81,11 +128,22 @@ export const App = (): JSX.Element => {
               &times;
             </button>
           )}
-          <h2>Welcome to the GoPasswordless Demo!</h2>
+          <h2>See how your app could look with GoPasswordless</h2>
           <p>
             Play around with our configurations to see how you can customise the
             login box.
           </p>
+          <h4>Presets</h4>
+          <select
+            value={selectedPreset}
+            onChange={(e) => selectPreset(e.target.value)}
+          >
+            {presets.map((p) => (
+              <option key={p.name} value={p.name}>
+                {p.name}
+              </option>
+            ))}
+          </select>
           <h4>Mode</h4>
           <select
             value={mode}
@@ -97,6 +155,18 @@ export const App = (): JSX.Element => {
             <option value="dark">Dark</option>
             <option value="glass">Glass</option>
           </select>
+          <h4>Logo URL</h4>
+          <input
+            type="text"
+            value={logoUrl}
+            onChange={(e) => setLogoUrl(e.target.value)}
+          />
+          <h4>Primary Colour</h4>
+          <input
+            type="text"
+            value={primaryColour}
+            onChange={(e) => setPrimaryColour(e.target.value)}
+          />
         </div>
         <div className="Container">
           {!isMobile && <GoPasswordlessWidgetComponent />}
